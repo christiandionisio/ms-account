3a913ea7dc11c880a8d6f9f35a40872b08c0f302
Christian Dionisio <christiandionisio9432@gmail.com>
2022-08-09 10:19:43 -0500

    AccountControllerTest added

--- src/test/java/com/example/msaccount/controller/AccountControllerTest.java
+++ src/test/java/com/example/msaccount/controller/AccountControllerTest.java
@@ -1,11 +1,7 @@
 package com.example.msaccount.controller;
 
-import com.example.msaccount.dto.AccountCustomerDto;
-import com.example.msaccount.dto.AccountDto;
-import com.example.msaccount.error.AccountCustomerWithoutCreditCardRequiredException;
-import com.example.msaccount.error.AccountInvalidBalanceException;
-import com.example.msaccount.error.AccountToBusinessCustomerNotAllowedExecption;
-import com.example.msaccount.error.PersonalCustomerHasAccountException;
+import com.example.msaccount.dto.*;
+import com.example.msaccount.error.*;
 import com.example.msaccount.models.Account;
 import com.example.msaccount.service.IAccountService;
 import org.junit.jupiter.api.BeforeEach;
@@ -172,6 +168,96 @@ class AccountControllerTest {
                 .hasSize(1);
     }
 
+    @Test
+    @DisplayName("Add holder to account")
+    void addHolderInAccount() {
+        Mockito.when(accountService.addHolders(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(Mono.just(getAccountWithHoldersTest()));
+
+        webClient.post().uri("/accounts/addHolder")
+                .body(Mono.just(new HolderAccountDto("1", "2")), HolderAccountDto.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(ResponseTemplateDto.class);
+    }
+
+    @Test
+    @DisplayName("Add holder to account with AccountNotSupportedForMultipleHoldersException")
+    void addHolderInAccountWithAccountNotSupportedForMultipleHoldersException() {
+        Mockito.when(accountService.addHolders(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(Mono.error(new AccountNotSupportedForMultipleHoldersException()));
+
+        webClient.post().uri("/accounts/addHolder")
+                .body(Mono.just(new HolderAccountDto("1", "2")), HolderAccountDto.class)
+                .exchange()
+                .expectStatus().isForbidden();
+    }
+
+    @Test
+    @DisplayName("Add holder to account with HolderAlredyExistInAccountEException")
+    void addHolderInAccountWithHolderAlredyExistInAccountEException() {
+        Mockito.when(accountService.addHolders(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(Mono.error(new HolderAlredyExistInAccountEException()));
+
+        webClient.post().uri("/accounts/addHolder")
+                .body(Mono.just(new HolderAccountDto("1", "2")), HolderAccountDto.class)
+                .exchange()
+                .expectStatus().isForbidden();
+    }
+
+    @Test
+    @DisplayName("Add holder to account with GeneralException")
+    void addHolderInAccountWithGeneralException() {
+        Mockito.when(accountService.addHolders(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(Mono.error(new Exception()));
+
+        webClient.post().uri("/accounts/addHolder")
+                .body(Mono.just(new HolderAccountDto("1", "2")), HolderAccountDto.class)
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+    @Test
+    @DisplayName("Read account")
+    void read() {
+        Mockito.when(accountService.findById(Mockito.anyString()))
+                .thenReturn(Mono.just(getAccountTest()));
+
+        webClient.get().uri("/accounts/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Account.class)
+                .isEqualTo(getAccountTest());
+    }
+
+    @Test
+    @DisplayName("Get balance available service")
+    void getBalanceAvailable() {
+        Mockito.when(accountService.getBalance(Mockito.anyString()))
+                .thenReturn(Mono.just(new BalanceDto(BigDecimal.valueOf(100), "PEN")));
+
+        webClient.get().uri("/accounts/balance/1")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    @DisplayName("Find by customer owner")
+    void findByCustomerOwner() {
+        List<Account> accountsMock = new ArrayList<>();
+        accountsMock.add(getAccountTest());
+
+        Mockito.when(accountService.findByCustomerOwnerId(Mockito.anyString()))
+                .thenReturn(Flux.fromIterable(accountsMock));
+
+        webClient.get().uri("/accounts/findByCustomerOwnerId/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Account.class)
+                .hasSize(1);
+    }
+
+
     private Account getAccountTest() {
         Account account = new Account();
         account.setAccountId("1");
@@ -182,5 +268,12 @@ class AccountControllerTest {
         account.setCustomerOwnerId("123");
         account.setCustomerOwnerType("PERSONAL");
         return account;
+    }
+
+    private AccountWithHoldersDto getAccountWithHoldersTest() {
+        List<String> holders = new ArrayList<>();
+        holders.add("1");
+        holders.add("2");
+        return new AccountWithHoldersDto(getAccountTest(), holders);
     }
 }
