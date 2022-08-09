179aba79a9f0f8cab12ddb4fee43db48b039ee1b
Christian Dionisio <christiandionisio9432@gmail.com>
2022-08-09 11:24:55 -0500

    AccountConfigurationControllerTest added

--- src/test/java/com/example/msaccount/controller/AccountConfigurationControllerTest.java
+++ src/test/java/com/example/msaccount/controller/AccountConfigurationControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.msaccount.controller;
 
+import com.example.msaccount.dto.AccountConfigurationDto;
 import com.example.msaccount.models.AccountConfiguration;
 import com.example.msaccount.service.IAccountConfigurationService;
 import org.junit.jupiter.api.DisplayName;
@@ -15,7 +16,6 @@ import reactor.core.publisher.Mono;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AccountConfigurationControllerTest {
@@ -50,6 +50,61 @@ class AccountConfigurationControllerTest {
                 .thenReturn(Mono.just(accountConfiguration));
 
         webClient.get().uri("/accounts/configurations/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(AccountConfiguration.class)
+                .isEqualTo(accountConfiguration);
+    }
+
+    @Test
+    @DisplayName("Create configuration")
+    void create() {
+        AccountConfigurationDto accountConfiguration = new AccountConfigurationDto();
+        Mockito.when(service.create(Mockito.any(AccountConfiguration.class)))
+                .thenReturn(Mono.just(getAccountConfigurationTest()));
+
+        webClient.post().uri("/accounts/configurations")
+                .body(Mono.just(accountConfiguration), AccountConfigurationDto.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(AccountConfiguration.class)
+                .isEqualTo(getAccountConfigurationTest());
+    }
+
+    @Test
+    @DisplayName("Update configuration")
+    void update() {
+        AccountConfigurationDto accountConfiguration = new AccountConfigurationDto();
+        Mockito.when(service.update(Mockito.any(AccountConfiguration.class)))
+                .thenReturn(Mono.just(getAccountConfigurationTest()));
+
+        webClient.put().uri("/accounts/configurations")
+                .body(Mono.just(accountConfiguration), AccountConfigurationDto.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(AccountConfiguration.class)
+                .isEqualTo(getAccountConfigurationTest());
+    }
+
+    @Test
+    @DisplayName("Delete configuration")
+    void delete() {
+        Mockito.when(service.delete(Mockito.anyString()))
+                .thenReturn(Mono.empty());
+
+        webClient.delete().uri("/accounts/configurations/1")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    @DisplayName("Find configuration by account type and name")
+    void findByAccountTypeAndName() {
+        AccountConfiguration accountConfiguration = getAccountConfigurationTest();
+        Mockito.when(service.findByAccountTypeAndName(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(Mono.just(accountConfiguration));
+
+        webClient.get().uri("/accounts/configurations/searchByAccountTypeAndName?accountType=1&name=1")
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(AccountConfiguration.class)
